8d3b28587ef3e0a5822f36d2e0b46d4396e07d4b
Getro Naissance <getro89@gmail.com>
2016-12-20 11:41:02 -0800

    added a shoe store and a shoe brand page.

--- app.rb
+++ app.rb
@@ -1,3 +1,11 @@
+require("sinatra")
+require("sinatra/reloader")
+also_reload("lib/**/*.rb")
 require("sinatra/activerecord")
-require(".lib/store")
-require(".lib/shoe_brand")
+require("./lib/store")
+require("./lib/brand")
+require("pg")
+
+get('/') do
+  erb(:index)
+end
